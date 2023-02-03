c8fdf6759caccd001507b970d3fb326175c10e44
Matheus <matheus_lsilva@hotmail.com>
2023-02-03 13:40:00 -0300

    data fetching

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Component } from 'react';
 //stateful component
 class App extends Component {
   state = {
-    counter: 0,
     posts: [
       {
         id: 1,
@@ -26,37 +25,44 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.handleTimeOut();
+    this.loadPosts();
   }
 
-  componentDidUpdate() {
-    //loop
-    // this.handleTimeOut();
-  }
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-  handleTimeOut = () => {
-    const { posts, counter } = this.state;
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-    posts[0].title = 'Alterando título';
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
-    setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 1000);
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({
+      posts: postsAndPhotos,
+    });
   };
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post" key={post.id}>
+              <img src={post.cover} alt={post.title} />
+              <div className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
