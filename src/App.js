23d90fac37b02241e81a02410c08effff2d6300d
Matheus <matheus_lsilva@hotmail.com>
2023-02-02 23:38:44 -0300

    stateful component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+
+import logo from './logo.svg';
+
+//stateful component
+class App extends Component {
+  state = {
+    name: 'Matheus Silva',
+    counter: 0,
+  };
+
+  handleParagraphClick = () => {
+    this.setState({ name: 'Matheus Lima' });
+  };
+
+  handleAnchorClick = (event) => {
+    event.preventDefault();
+
+    const { counter } = this.state;
+
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleParagraphClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAnchorClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
