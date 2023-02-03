8e7b85083b513263a4b1401fddb2bfd76aa80703
Matheus <matheus_lsilva@hotmail.com>
2023-02-03 10:42:06 -0300

    state with arrays and objects

--- src/App.js
+++ src/App.js
@@ -2,47 +2,39 @@ import './App.css';
 
 import { Component } from 'react';
 
-import logo from './logo.svg';
-
 //stateful component
 class App extends Component {
   state = {
-    name: 'Matheus Silva',
-    counter: 0,
-  };
-
-  handleParagraphClick = () => {
-    this.setState({ name: 'Matheus Lima' });
-  };
-
-  handleAnchorClick = (event) => {
-    event.preventDefault();
-
-    const { counter } = this.state;
-
-    this.setState({ counter: counter + 1 });
+    posts: [
+      {
+        id: 1,
+        title: 'título 1',
+        body: 'Corpo 1',
+      },
+      {
+        id: 2,
+        title: 'título 2',
+        body: 'Corpo 2',
+      },
+      {
+        id: 3,
+        title: 'título 3',
+        body: 'Corpo 3',
+      },
+    ],
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleParagraphClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAnchorClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
