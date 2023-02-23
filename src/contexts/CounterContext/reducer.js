93fba4d5ed42360934bba1df03d4db3318cd694c
Matheus <matheus_lsilva@hotmail.com>
2023-02-22 21:57:45 -0300

    set flag to sinalize error

--- src/contexts/CounterContext/reducer.js
+++ src/contexts/CounterContext/reducer.js
@@ -4,19 +4,19 @@ import * as actions from "./actions";
 export const counterReducer = (state, action) => {
     switch (action.type) {
         case actions.INCREASE:
-            return { ...state, counter: state.counter + 1 };
+            return { ...state, counter: state.counter + 1, error: false };
         case actions.DECREASE:
-            return { ...state, counter: state.counter - 1 };
+            return { ...state, counter: state.counter - 1, error: false };
         case actions.RESET:
             return { ...initialState };
         case actions.SET_COUNTER:
-            return { ...state, ...action.payload };
+            return { ...state, ...action.payload, error: false };
         case actions.ASYNC_INCREASE_START:
-            return { ...state, loading: true };
+            return { ...state, loading: true, error: false };
         case actions.ASYNC_INCREASE_END:
-            return { ...state, loading: false, counter: state.counter + 1 };
+            return { ...state, loading: false, counter: state.counter + 1, error: false };
         case actions.ASYNC_INCREASE_ERROR:
-            return { ...state, loading: false };
+            return { ...state, loading: false, error: true };
     }
 
     return state;
