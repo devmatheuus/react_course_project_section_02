9ee475095620246b889fa3516764ab1c41b675eb
Matheus <matheus_lsilva@hotmail.com>
2023-02-22 21:58:09 -0300

    handling context

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -5,12 +5,36 @@ import { Heading } from "../../components/Heading";
 import { useCounterContext } from "../../contexts/CounterContext";
 
 export const Home = () => {
-    const [, actions] = useCounterContext();
+    const [state, actions] = useCounterContext();
+
+    const handleError = () => {
+        actions
+            .asyncError()
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error.name, ":", error.message));
+    };
 
     return (
         <section className="container">
             <Heading />
-            <Button onButtonClick={actions.increase}>INCREASE</Button>
+            <Button onButtonClick={actions.increase} disabled={state.loading}>
+                INCREASE
+            </Button>
+            <Button onButtonClick={actions.decrease} disabled={state.loading}>
+                DECREASE
+            </Button>
+            <Button onButtonClick={actions.reset} disabled={state.loading}>
+                RESET
+            </Button>
+            <Button onButtonClick={() => actions.setCounter({ counter: 999 })} disabled={state.loading}>
+                SET COUNTER
+            </Button>
+            <Button onButtonClick={actions.asyncIncrease} disabled={state.loading}>
+                ASYNC INCREASE
+            </Button>
+            <Button onButtonClick={handleError} disabled={state.loading}>
+                ASYNC ERROR
+            </Button>
         </section>
     );
 };
