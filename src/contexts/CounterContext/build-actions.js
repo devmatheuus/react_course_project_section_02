9a6674d11d82e6753fb72562cb2f29b5e2cdcaa3
Matheus <matheus_lsilva@hotmail.com>
2023-02-22 21:57:13 -0300

    finishing dispatch functions handlers

--- src/contexts/CounterContext/build-actions.js
+++ src/contexts/CounterContext/build-actions.js
@@ -3,5 +3,33 @@ import * as actions from "./actions";
 export const buildActions = (dispatch) => {
     return {
         increase: () => dispatch({ type: actions.INCREASE }),
+        decrease: () => dispatch({ type: actions.DECREASE }),
+        reset: () => dispatch({ type: actions.RESET }),
+        setCounter: (payload) => dispatch({ type: actions.SET_COUNTER, payload }),
+        asyncIncrease: () => asyncIncreaseFn(dispatch),
+        asyncError: () => asyncErrorFn(dispatch),
     };
+};
+
+const asyncIncreaseFn = async (dispatch) => {
+    dispatch({ type: actions.ASYNC_INCREASE_START });
+
+    return await new Promise((resolve) => {
+        setTimeout(() => {
+            dispatch({ type: actions.ASYNC_INCREASE_END });
+
+            resolve("RESOLVED!");
+        }, 2000);
+    });
+};
+
+const asyncErrorFn = async (dispatch) => {
+    dispatch({ type: actions.ASYNC_INCREASE_START });
+
+    return await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            dispatch({ type: actions.ASYNC_INCREASE_ERROR });
+            reject(new Error("ERROR!"));
+        }, 2000);
+    });
 };
